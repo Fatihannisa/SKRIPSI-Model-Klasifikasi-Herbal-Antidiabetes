aa2d00cb40f65705a9a07ac6538a6b02379a3d3c
Fatihannisa Listy Zulmi_034 <114486927+Fatihannisa@users.noreply.github.com>
2026-09-18 11:27:48 +0700

    Update sistem_klasifikasi_herbal.py

--- sistem_klasifikasi_herbal.py
+++ sistem_klasifikasi_herbal.py
@@ -635,15 +635,6 @@ if st.session_state.page == "upload":
 
     with col1:
         st.subheader("📷 Unggah Citra Daun")
-        st.markdown("""
-            <style>
-            [data-testid="stFileUploader"] label {
-                font-size: 20px !important;
-                font-weight: 500 !important;
-            }
-            </style>
-        """, unsafe_allow_html=True)
-        
         uploaded_file = st.file_uploader(
             "Pilih file foto daun (JPG, PNG, WEBP) dengan ukuran file maksimal 6 MB",
             type=["jpg", "jpeg", "png", "webp"],
